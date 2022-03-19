9174ce5f6cdc8362a82cee8ce0f4abdcf10e33f2
Alexey-Joos <99196626+Alexey-Joos@users.noreply.github.com>
2022-03-19 15:26:24 +0300

    Study1 Except

--- ScanNC.py
+++ ScanNC.py
@@ -1,8 +1,23 @@
+import numpy as np
 
-def main():
-    pass
 
-def print_line():
-    pass
+n = int(input())
+k = int(input())
+arr = []
+new_arr = []
 
-mani()
+for i in range(n):
+    arr.append(i)
+
+
+while n % k != 0:
+    n -= 1
+    k -= 1
+    tmp = []
+    for j in range(n//k):
+        tmp.append(arr[j])
+        arr.pop(j)
+    new_arr.append(tmp)
+
+
+print(new_arr)
