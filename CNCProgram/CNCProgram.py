e8ab1544dcea0711136c23564fce024277d76a84
Alexey-Joos <99196626+Alexey-Joos@users.noreply.github.com>
2022-04-09 17:49:55 +0300

    Create main window with parametr

---
+++ CNCProgram/CNCProgram.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+main_window = tk.Tk()
+photo = tk.PhotoImage(file='icon.png')
+main_window.iconphoto(False, photo)
+main_window.title("CNCProgram")
+main_window.geometry("1024x768")
+
+
+main_window.mainloop()
