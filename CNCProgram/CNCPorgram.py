9998e2e3f5aa6182d04b0b9ac6cfff8de1975016
Alexey-Joos <99196626+Alexey-Joos@users.noreply.github.com>
2022-04-09 17:41:53 +0300

    Delete CNCPorgram.py

--- CNCProgram/CNCPorgram.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-main_window = tk.Tk()
-photo = tk.PhotoImage(file='icon.png')
-main_window.title("ProgramCNC")
-main_window.geometry("1024x768")
-main_window.resizable(False, False)
-
-main_window.mainloop()
